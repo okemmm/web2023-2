b56394737e5e5ff90917533d14dd6d23e0fa6409
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-03 10:02:50 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,6 +1,12 @@
-alert('Hello, Osamu!');　  /* JSは'' ""　のどちらでも良い */
+alert('Hello, Yuta!');　  /* JSは'' ""　のどちらでも良い */
 alert(1+1);
 alert(Math.sqrt(1+1));
 alert(Math.sqrt(2+3));
 alert(Math.sqrt(1/0));
 alert(Math.sqrt(0/0));
+alert(Math.sqrt(2+3));
+alert(Math.sqrt('2'+'3'));
+alert(Math.sqrt(Number('2')+Number('3'));
+alert(Math.sqrt('2'+3));
+alert(Math.sqrt(('2')+3));
+alert(Math.sqrt(Number('2')+3));
