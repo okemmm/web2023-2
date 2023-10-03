840028d7199664778c1a9e51286365a226d1e1e3
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-03 10:35:25 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,10 +2,10 @@ alert('Hello, Yuta!');  /* JSは'' ""　のどちらでも良い */
 alert(1+1);
 alert(Math.sqrt(1+1));
 alert(Math.sqrt(2+3));
-alert(Math.sqrt(1/0));  // 無限大の平方根はNaNになります
-alert(Math.sqrt(0/0));  // NaNの平方根はNaNになります
-alert(Math.sqrt(2+3));
-alert(Math.sqrt('2'+'3'));  // 文字列の連結'23'の平方根はエラーになります
-alert(Math.sqrt(Number('2')+Number('3')));  // 修正: 閉じ括弧を追加
-alert(Math.sqrt('2'+3));  // 文字列の連結'23'の平方根はエラーになります
-alert(Math.sqrt(Number('2')+3));
+alert(1/0);
+alert(0/0);
+alert(2+3);
+alert("2"+"3");
+alert(Number("2")+Number("3"));
+alert("2"+3);
+alert(Number("2")+3);
