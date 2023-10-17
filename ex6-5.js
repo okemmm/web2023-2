f66efc4f9d3143423317a244f4b402f10e7fb708
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-17 09:30:49 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,4 +1,4 @@
-var who65 = 'world';
+<!--var who65 = 'world';
 
 function sayhello65() {
   alert('Hello,' + who65 + '!');
@@ -11,3 +11,4 @@ function taro65() {
 function hanako65() {
   who65 = '花子';
 }
+-->
