25e5ab4e4227d79e66bcd2edbe1c20e254c59299
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-17 10:28:13 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,38 +1,12 @@
-var who65 = 'world';function sayhello65(){ alert('Hello,'+who65+'!');
+var who65 ='world';
 
-function taro65(){who65 = '太郎';}
-function hanako65(){who65 = '花子';}
+function sayhello65(){ alert('Hello,'+ who65 +'!');}
 
-var who66='world';function sayhello66(){alert('Hello,'+who66+'!');}
-function taro66(){alert(who66='太郎'+が来た+'!');}
-function taro66(){alert(who66='太郎'+が来た+'!');}
+var who1 ='someone';
 
+function coming1(){ alert(who1 + 'が来た!');}
 
 
-/*var who65 = 'world';
+function taro65(){ who65 = '太郎'; who1 = '太郎';}
 
-function sayhello65() {
-  alert('Hello,' + who65 + '!');
-}
-
-function taro66() {
-  who65 = '太郎';
-  showMessage(who65 + 'が来た');
-}
-
-function hanako66() {
-  who65 = '花子';
-  showMessage(who65 + 'が来た');
-}
-
-function showMessage(message) {
-  alert(message);
-}
-
-function sayhello61() {
-  alert('Hello, Function!');
-}
-
-function saygoodbye() {
-  alert('Goodbye');
-} */
+function hanako65(){ who65 ='花子'; who1 ='花子';}
