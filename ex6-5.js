3e6bd058ce3eea4c86cf614f3f49ec78bc26af71
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-17 10:59:58 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,12 +1,20 @@
 var who65 ='world';
 
-function sayhello65(){ alert('Hello,'+ who65 +'!');}
+function sayhello65(){
+  alert('Hello,'+ who65 +'!');
+}
 
 var who1 ='someone';
 
-function coming1(){ alert(who1 + 'が来た!');}
+function coming1(){
+  alert(who1 + 'が来た!');
+}
 
 
-function taro65(){ who65 = '太郎'; who1 = '太郎';}
+function taro65(){
+  who65 = '太郎'; who1 = '太郎';
+}
 
-function hanako65(){ who65 ='花子'; who1 ='花子';}
+function hanako65(){
+  who65 ='花子'; who1 ='花子';
+}
