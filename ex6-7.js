855f841ce37109ce9a3200ab116b9bc01654f7b4
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-23 10:04:57 +0900

    Update ex6-7.js

--- ex6-7.js
+++ ex6-7.js
@@ -5,23 +5,3 @@ function sayhello67(){
 function someone(x){
   who67 = x;
 }
-/*
-var who65='world';
-
-function sayhello65(){
-  alert('Hello,' + who65 + '!');
-}
-function taro65(){
-  who65 = '太郎';
-}
-function hanako65(){
-  who65 = '花子';
-}
-
-function taro66(){
-  alert('太郎' + 'が来た');
-}
-function hanako66(){
-  alert('花子' + 'が来た');
-}
-*/
