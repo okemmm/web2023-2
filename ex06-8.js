1f6762d9c3d3f59b0ba8a0334db462b08477e7d1
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-24 10:01:38 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,12 +1,15 @@
 function sayhello68(){
-  alert('Hello,' + document.getElementByld('who68').innerHTML'!');
+  alert('Hello,' + document.getElementById('who68').innerHTML + '!');
 }
+
 function taro68(){
-  document.getElementByld('who68').innerHTML = '太郎';
+  document.getElementById('who68').innerHTML = '太郎';
 }
+
 function hanako68(){
-  document.getElementByld('who68').innerHTML = '花子';
+  document.getElementById('who68').innerHTML = '花子';
 }
+
 function gazo688(){
   document.getElementByld('who688').innerHTML = '<img src="./image/bulbon.gif" alt="オン">';
 }
