ef72ba60d76da989ab446bc5286ab3a29459d764
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-31 06:56:54 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,3 +1,8 @@
 function imgchange1() {
-  document.getElementById('logoImage').src = './image/josai4.png'; // idを修正
+  document.getElementById('logoImage').src = './image/josai4.png';
 }
+
+function imgchange2() {
+  document.getElementById('logoImage').src = './image/josai5.png'; // 元の画像のパスに変更
+}
+
