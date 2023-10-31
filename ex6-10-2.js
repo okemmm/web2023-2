6ea9e23dcdf3aa9d3c2c83dd92e7538ab58ddfe3
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-31 09:20:14 +0900

    Create ex6-10-2.js

---
+++ ex6-10-2.js
@@ -0,0 +1,7 @@
+function imgchange1() {
+  document.getElementById('logoImage').src = './image/josai4.png';
+}
+
+function imgchange2() {
+  document.getElementById('logoImage').src = './image/josai5.png'; // 元の画像に戻す
+}
