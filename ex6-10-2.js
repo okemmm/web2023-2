3241942410ec8509b2ec744e437064e4ee986867
okemmm <131091828+okemmm@users.noreply.github.com>
2023-10-31 09:21:47 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,7 +1,7 @@
-function imgchange1() {
-  document.getElementById('logoImage').src = './image/josai4.png';
+function imgchange2() {
+  document.getElementById('logoImage2').src = './image/josai6.png';
 }
 
-function imgchange2() {
-  document.getElementById('logoImage').src = './image/josai5.png'; // 元の画像に戻す
+function imgchange3() {
+  document.getElementById('logoImage2').src = './image/josai7.png'; // 元の画像に戻す
 }
