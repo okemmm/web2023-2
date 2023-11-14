31eb64475f4615f70800502bbaeb5d8c658deafe
okemmm <131091828+okemmm@users.noreply.github.com>
2023-11-14 09:57:59 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,3 +1,18 @@
-function photograph3(){
-<img src='./image/Night.png' id="img3" alt="photochange" class="photograph">
-setTimeout("photograph3()",3000);}
+//function photograph3(){
+//<img src='./image/Night.png' id="img3" alt="photochange" class="photograph">
+//setTimeout("photograph3()",3000);}
+
+
+var originalImageSrc = './image/Morning.png'; // 元の画像のURL
+
+function photograph3() {
+  var imgElement = document.getElementById("img3");
+  imgElement.src = './image/Night.png';
+  imgElement.alt = "photochange";
+
+  // 3秒後に元の画像に戻す
+  setTimeout(function() {
+    imgElement.src = originalImageSrc;
+    imgElement.alt = "photochange";
+  }, 3000);
+}
