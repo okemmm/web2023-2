69b3ab353c846423b6445a40625562b3a791e9b0
okemmm <131091828+okemmm@users.noreply.github.com>
2023-11-21 09:06:15 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,10 +1,5 @@
 function add(){
-  var x = document.getElementByld('input1').value;
-  var y = document.getElementByld('input2').value;
-  alert(x+y);
-}
-function add(){
-  var x = parseInt(document.getElementById('input1').value);
-  var y = parseInt(document.getElementById('input2').value);
+  var x = Number(document.getElementById('input1').value);
+  var y = Number(document.getElementById('input2').value);
   alert(x + y);
 }
