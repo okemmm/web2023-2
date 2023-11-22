06cc671d6238f618b914bdb3b255b7b6c6e87e12
okemmm <131091828+okemmm@users.noreply.github.com>
2023-11-22 09:53:44 +0900

    Update ex07-8.js

--- ex07-8.js
+++ ex07-8.js
@@ -1,5 +1,5 @@
-function nazo77-2() {
-  if (document.getElementById('answer77-2').value == '夢') {
+function nazo772() {
+  if (document.getElementById('answer772').value == '夢') {
     alert('あたり!');
   } else {
     alert('はずれ!');
