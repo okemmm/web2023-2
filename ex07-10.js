5895a11fa4f270723f4eca587e306108ae07d47b
okemmm <131091828+okemmm@users.noreply.github.com>
2023-11-28 09:18:05 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,16 +1,16 @@
-function BMI() {
-    var height = Number(document.getElementById('height').value) / 100; // 身長をメートルに変換
-    var weight = Number(document.getElementById('weight').value);
-    var bmi = weight / (height * height); // BMIの計算
+function BMI2() {
+    var height2 = Number(document.getElementById('height2').value) / 100; // 身長をメートルに変換
+    var weight2 = Number(document.getElementById('weight2').value);
+    var bmi2 = weight2 / (height2 * height2); // BMIの計算
 
     var classification;
-    if (bmi < 18.5) {
+    if (bmi2 < 18.5) {
         classification = '低体重';
-    } else if (bmi < 25) {
+    } else if (bmi2 < 25) {
         classification = '普通';
     } else {
         classification = '肥満';
     }
 
-    alert('あなたのBMIは ' + bmi + ' です。これは「' + classification + '」の範囲に分類されます。');
+    alert('あなたのBMIは ' + bmi2 + ' です。これは「' + classification + '」の範囲に分類されます。');
 }
