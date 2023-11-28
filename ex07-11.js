556e7e70a41dec4dda2fa71761f99687d1a67296
okemmm <131091828+okemmm@users.noreply.github.com>
2023-11-28 09:56:08 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -10,3 +10,17 @@ function determineLeapYear() {
 
     document.getElementById('result2').innerText = '西暦' + year + '年は' + result2 + 'です。';
 }
+
+
+function determineLeapYear() {
+    var year = Number(document.getElementById('yearInput').value);
+    var result;
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        result = '閏年';
+    } else {
+        result = '平年';
+    }
+
+    document.getElementById('result2').innerText = '西暦' + year + '年は' + result + 'です。';
+}
